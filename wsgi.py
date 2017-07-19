1a03166db402777db6776aa44289cced60add52f
Victor Mireles Chavez <victor.mireles-chavez@semantic-web.com>
2017-07-19 14:14:01 +0200

    a change

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,10 @@ def hello():
     greeting = "Hello From Within Openshift \n "
     #greeting += os.environ["TEST_ENV"]
 
-
-    config_file = "/configFiles/my-config-file"
-    f = open(config_file,"rt")
-    config_string = f.read()
-    greeting += config_string.strip()
+    # config_file = "/configFiles/my-config-file"
+    # f = open(config_file,"rt")
+    # config_string = f.read()
+    # greeting += config_string.strip()
 
 
     print(greeting)
