0c3de1c6f3f89e276dd92555f52fe46fc56461b6
Victor Mireles Chavez <victor.mireles-chavez@semantic-web.com>
2017-07-19 12:56:31 +0200

    a change

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,16 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    greeting = "Hello From Within Openshift"
+    greeting = "Hello From Within Openshift \n "
     #greeting += os.environ["TEST_ENV"]
+
+
+    config_file = "/configFiles/my-config-file"
+    f = open(config_file,"rt")
+    config_string = f.read()
+    greeting += config_string.strip()
+
+
     print(greeting)
     return greeting
 
